28808f1c7c14efc9f5fd0b71b2876033212b4155
undervineg <undervineg@gmail.com>
2018-06-17 03:49:03 +0900

    파일만 추가..

---
+++ PhotoApp/PhotoAppTests/NetworkModuleTests.swift
@@ -0,0 +1,27 @@
+//
+//  NetworkModuleTests.swift
+//  PhotoAppTests
+//
+//  Created by 심 승민 on 2018. 6. 15..
+//  Copyright © 2018년 심 승민. All rights reserved.
+//
+
+import XCTest
+
+class NetworkModuleTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func test_fetch_doodleImages() {
+        
+    }
+    
+}
---
+++ PhotoApp/PhotoAppTests/PhotoAppTests.swift
@@ -0,0 +1,23 @@
+//
+//  PhotoAppTests.swift
+//  PhotoAppTests
+//
+//  Created by 심 승민 on 2018. 6. 15..
+//  Copyright © 2018년 심 승민. All rights reserved.
+//
+
+import XCTest
+
+class PhotoAppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+
+}

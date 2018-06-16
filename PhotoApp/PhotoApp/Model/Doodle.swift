733dad9c315af730aa24fd0ff1035b4df28d79d2
undervineg <undervineg@gmail.com>
2018-06-17 03:37:45 +0900

    [추가] 표시할 모델 속성 추가

---
+++ PhotoApp/PhotoApp/Model/Doodle.swift
@@ -0,0 +1,15 @@
+//
+//  Doodles.swift
+//  PhotoApp
+//
+//  Created by 심 승민 on 2018. 5. 21..
+//  Copyright © 2018년 심 승민. All rights reserved.
+//
+
+import Foundation
+
+struct Doodle: Decodable {
+    let title: String
+    let date: String
+    let image: String
+}

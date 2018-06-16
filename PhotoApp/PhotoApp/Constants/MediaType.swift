e010caf6c53bd4308cba592312fa55ceb17bb56f
undervineg <undervineg@gmail.com>
2018-06-17 03:46:34 +0900

    [추가] 미디어 타입 추가

---
+++ PhotoApp/PhotoApp/Constants/MediaType.swift
@@ -0,0 +1,14 @@
+//
+//  MediaType.swift
+//  PhotoApp
+//
+//  Created by 심 승민 on 2018. 6. 17..
+//  Copyright © 2018년 심 승민. All rights reserved.
+//
+
+import UIKit
+
+enum MediaType {
+    case video(URL)
+    case image(UIImage)
+}

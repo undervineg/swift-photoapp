387ee963b46c5415a5e5b6fd81bb1abea6595f91
undervineg <undervineg@gmail.com>
2018-06-17 03:46:57 +0900

    파일 옮김

---
+++ PhotoApp/PhotoApp/Constants/ViewConfig.swift
@@ -0,0 +1,14 @@
+//
+//  CollectionView+Config.swift
+//  PhotoApp
+//
+//  Created by 심 승민 on 2018. 4. 17..
+//  Copyright © 2018년 심 승민. All rights reserved.
+//
+
+import UIKit
+
+struct ViewConfig {
+    static let thumbnailSize: CGSize = CGSize(width: 100, height: 100)
+    static let videoSize: CGSize = CGSize(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+}
